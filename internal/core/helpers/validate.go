d5c0c47e2de434be0aa48eab9f6c75bf0ca0ac6c
Tchisom17 <kamsiyochukwu1@gmail.com>
2022-04-19 23:03:00 +0100

    Validate fields and check for errors

---
+++ internal/core/helpers/validate.go
@@ -0,0 +1,13 @@
+package helpers
+
+import "fmt"
+
+// ValidationError defines error that occur due to validation
+type ValidationError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func (v ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", v.Field, v.Message)
+}
