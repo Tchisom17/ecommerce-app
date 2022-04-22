c2bbd796217d40f411bc68208e1e3470bd6a9666
Tchisom17 <kamsiyochukwu1@gmail.com>
2022-04-22 09:13:06 +0100

    Contain the JSON response

--- pkg/utils/response.go
+++ pkg/utils/response.go
@@ -1,53 +1,19 @@
 package utils
 
-// Result struct for creating response
-type Result struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message,omitempty"`
-	Error   interface{} `json:"error,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Token   string      `json:"token,omitempty"`
-	Count   int64       `json:"count,omitempty"`
-}
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
+)
 
-// ReturnErrorResult return error object
-func (r *Result) ReturnErrorResult(error string) Result {
-	return Result{
-		Success: false,
-		Error:   error,
+func JSON(c *gin.Context, message string, status int, data interface{}, errs []string) {
+	responsedata := gin.H{
+		"message":   message,
+		"data":      data,
+		"errors":    errs,
+		"status":    http.StatusText(status),
+		"timestamp": time.Now().Format("2006-01-02 15:04:05"),
 	}
-}
 
-// ReturnSuccessResult return success object
-func (r *Result) ReturnSuccessResult(data interface{}, message string) Result {
-	return Result{
-		Success: true,
-		Data:    data,
-		Message: message,
-	}
-}
-
-// ReturnSuccessMessage returns just success message
-func (r *Result) ReturnSuccessMessage(message string) Result {
-	return Result{
-		Success: true,
-		Message: message,
-	}
-}
-
-// ReturnBasicResult returns just object without message
-func (r *Result) ReturnBasicResult(data interface{}) Result {
-	return Result{
-		Success: true,
-		Data:    data,
-	}
-}
-
-// ReturnAuthResult returns both object and token
-func (r *Result) ReturnAuthResult(data interface{}, token string) Result {
-	return Result{
-		Success: true,
-		Token:   token,
-		Data:    data,
-	}
+	c.JSON(status, responsedata)
 }
